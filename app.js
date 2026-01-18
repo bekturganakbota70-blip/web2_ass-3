2172548eae6b1df0d674111716f9d6693cae5bd7
Akbota <bekturganakbota70@gmail.com>
2026-01-18 20:27:53 +0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const connectDB = require("./config/db");
+
+const app = express();
+
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/products", require("./routes/productRoutes"));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
